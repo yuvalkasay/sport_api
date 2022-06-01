58900bd6196ea64c2c07a497db9dbfb45c085cab
yuvalkasay <57451645+yuvalkasay@users.noreply.github.com>
2022-06-01 20:56:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+const PORT = process.env.PORT || 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const newspapers = [
+  {
+    name: "theguardian",
+    address: "https://www.theguardian.com/football",
+    base: "",
+  },
+  {
+    name: "espn",
+    address: "https://www.espn.com/soccer/",
+    base: "https://www.espn.com/soccer",
+  },
+  {
+    name: "skysports",
+    address: "https://www.skysports.com/football",
+    base: "",
+  },
+];
+
+const app = express();
+
+const articles = [];
+
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("transfer")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+
+      articles.push({
+        title,
+        url: newspaper.base + url,
+        source: newspaper.name,
+      });
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json("Welcome to my football transfer API");
+});
+
+app.get("/news", (req, res) => {
+  res.json(articles);
+});
+
+app.get("/news/:newspaperId", (req, res) => {
+  const newspaperId = req.params.newspaperId;
+
+  const newspaperAdress = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].address;
+  const newspaperBase = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].base;
+
+  axios
+    .get(newspaperAdress)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const specificArticles = [];
+
+      $('a:contains("transfer")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr("href");
+        specificArticles.push({
+          title,
+          url: newspaperBase + url,
+          source: newspaperId,
+        });
+      });
+      res.json(specificArticles);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
